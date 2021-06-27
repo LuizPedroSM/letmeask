750f07596a1dfccba46543b351371a01a301b6e8
Luiz Pedro <luizpsmoreira@gmail.com>
2021-06-26 23:17:28 -0300

    :hammer: update type, add handles, update question option
    Update type QuestionProps. Add question options. Add handleCheckQuestionAnswered and handleHighlightQueston to AdminRoom.

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -9,6 +9,8 @@ import { Question } from "../components/Question";
 
 import logoImg from "../assets/images/logo.svg";
 import deleteImg from "../assets/images/delete.svg";
+import checkImg from "../assets/images/check.svg";
+import answerImg from "../assets/images/answer.svg";
 import "../styles/room.scss";
 
 type RoomParams = {
@@ -32,6 +34,18 @@ export function AdminRoom() {
 		if (window.confirm("Tem certeza que deseja excluir esta pergunta?")) {
 			await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
 		}
+	}
+
+	async function handleCheckQuestionAnswered(questionId: string) {
+		await database
+			.ref(`rooms/${roomId}/questions/${questionId}`)
+			.update({ isAnswered: true });
+	}
+
+	async function handleHighlightQuestion(questionId: string) {
+		await database
+			.ref(`rooms/${roomId}/questions/${questionId}`)
+			.update({ isHighlighted: true });
 	}
 
 	return (
@@ -60,7 +74,27 @@ export function AdminRoom() {
 							key={question.id}
 							author={question.author}
 							content={question.content}
+							isAnswered={question.isAnswered}
+							isHighlighted={question.isHighlighted}
 						>
+							{!question.isAnswered && (
+								<>
+									<button
+										type="button"
+										onClick={() => handleCheckQuestionAnswered(question.id)}
+									>
+										<img src={checkImg} alt="Marcar pergunta como respondida" />
+									</button>
+
+									<button
+										type="button"
+										onClick={() => handleHighlightQuestion(question.id)}
+									>
+										<img src={answerImg} alt="Dar destaque à pergunta" />
+									</button>
+								</>
+							)}
+
 							<button
 								type="button"
 								onClick={() => handleDeleteQuestion(question.id)}

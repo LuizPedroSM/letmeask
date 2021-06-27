13f001199002542dbee14c076faacb320aa82845
Luiz Pedro <luizpsmoreira@gmail.com>
2021-06-27 16:23:58 -0300

    :mute: Remove log
    Remove log from firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -12,7 +12,6 @@ var firebaseConfig = {
 	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 	appId: process.env.REACT_APP_APP_ID,
 };
-console.log(firebaseConfig);
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
